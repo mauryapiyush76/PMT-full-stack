5181a960d4fecea251db8e1849f5ec745b55d935
mauryapiyush76 <mauryapiyush76@gmail.com>
2021-12-03 12:49:38 +0530

    First commit

---
+++ PMT-backend/src/main/java/io/piyush/pmt/PmtBackendApplication.java
@@ -0,0 +1,13 @@
+package io.piyush.pmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmtBackendApplication.class, args);
+	}
+
+}
---
+++ PMT-backend/src/test/java/io/piyush/pmt/PmtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.piyush.pmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
