5bd32b7a226ea7c86fe74f2e84e7c9388b8c6bdb
mauryapiyush76 <mauryapiyush76@gmail.com>
2021-12-09 10:49:24 +0530

    project entity

---
+++ PMT-backend/src/main/java/io/piyush/pmt/repositories/ProjectRepository.java
@@ -0,0 +1,18 @@
+package io.piyush.pmt.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import io.piyush.pmt.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+	@Override
+	default Iterable<Project> findAllById(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
