e66145c0f97dd76de3dbe56699d1f582c99c6a74
mauryapiyush76 <mauryapiyush76@gmail.com>
2021-12-29 23:09:18 +0530

    update operation

--- PMT-backend/src/main/java/io/piyush/pmt/services/ProjectService.java
+++ PMT-backend/src/main/java/io/piyush/pmt/services/ProjectService.java
@@ -41,4 +41,29 @@ public class ProjectService {
 		}
 		projectRepository.delete(project);
 	}
+
+	public Project updateProject(Project updatedProject, String projectId) {
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		if (project == null) {
+			throw new ProjectIdException(
+					"Cannot update project because no project exists with PROJECT ID '" + projectId + "'");
+		}
+		if (updatedProject.getDescription() != null) {
+			project.setDescription(updatedProject.getDescription());
+		}
+		if (updatedProject.getStart_date() != null) {
+			project.setStart_date(updatedProject.getStart_date());
+		}
+		if (updatedProject.getEnd_date() != null) {
+			project.setEnd_date(updatedProject.getEnd_date());
+		}
+		if (updatedProject.getProjectName() != null) {
+			project.setProjectName(updatedProject.getProjectName());
+		}
+		if (updatedProject.getId() != null) {
+			project.setId(updatedProject.getId());
+		}
+
+		return project;
+	}
 }
--- PMT-backend/src/main/java/io/piyush/pmt/web/ProjectController.java
+++ PMT-backend/src/main/java/io/piyush/pmt/web/ProjectController.java
@@ -1,21 +1,16 @@
 package io.piyush.pmt.web;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,5 +53,15 @@ public class ProjectController {
 	public ResponseEntity<?> deleteByProjectId(@PathVariable String projectId) {
 		projectService.deleteByProjectId(projectId);
 		return new ResponseEntity<String>("Project with PROJECT ID '" + projectId + "' deleted", HttpStatus.OK);
+	}
+
+	@PutMapping("/{projectId}")
+	public ResponseEntity<?> updateProject(@Valid @RequestBody Project project, @PathVariable String projectId,
+			BindingResult result) {
+		ResponseEntity<?> errorMap = validationMapService.getValidationErrorsMap(result);
+		if (errorMap != null)
+			return errorMap;
+		Project project2 = projectService.updateProject(project, projectId);
+		return new ResponseEntity<Project>(project2, HttpStatus.OK);
 	}
 }
