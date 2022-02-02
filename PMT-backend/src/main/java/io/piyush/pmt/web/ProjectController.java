03508a5ef8c55cf90a76ce137e80522a2f0343a1
Piyush Mourya <mauryapiyush76@gmail.com>
2022-02-02 13:06:23 +0530

    added some comments

--- PMT-backend/src/main/java/io/piyush/pmt/web/ProjectController.java
+++ PMT-backend/src/main/java/io/piyush/pmt/web/ProjectController.java
@@ -1,17 +1,11 @@
 package io.piyush.pmt.web;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- PMT-backend/src/test/java/io/piyush/pmt/PmtBackendApplicationTests.java
+++ PMT-backend/src/test/java/io/piyush/pmt/PmtBackendApplicationTests.java
@@ -1,5 +1,7 @@
 package io.piyush.pmt;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
